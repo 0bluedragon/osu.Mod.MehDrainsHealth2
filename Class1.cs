50d17755dfdb01c719e5bcdc15b2473524639915
Blue <colinosiel15@gmail.com>
2026-05-16 14:05:51 -0700

    Initial commit: working Meh Drains Health mod

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Scoring;
+using osu.Game.Rulesets;
+using osu.Framework.Localisation;
+
+namespace osu.Mod.MehDrainsHealth2
+{
+    public class ModMehDrainsHealth : osu.Game.Rulesets.Mods.Mod, IApplicableToHealthProcessor
+    {
+        public override string Name => "Meh Drains Health";
+        public override string Acronym => "MDH";
+
+        public override LocalisableString Description => "Lose health whenever you hit a Meh.";
+        public override double ScoreMultiplier => 1.0;
+
+        public void ApplyToHealthProcessor(HealthProcessor healthProcessor)
+        {
+            healthProcessor.NewJudgement += judgement =>
+            {
+                if (judgement.Type == HitResult.Meh)
+                    healthProcessor.Health.Value -= 0.05;
+            };
+        }
+    }
+}
